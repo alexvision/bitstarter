44c86bf7b33a4ebd9d0e1848de0bdf2524231680
Alex Manusu <alex.manusu@gmail.com>
2013-07-10 14:59:55 +1000

    web.js updated to load index.html

--- web.js
+++ web.js
@@ -2,9 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var fs = require('fs');
+    var buffer = new Buffer(fs.readFileSync("index.html"), 'utf-8');
+    response.send(buffer.toString());
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
